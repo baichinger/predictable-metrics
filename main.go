307a2edb35a49ede5fb6e786566e75754a672ff2
namratachaudhary <namratachaudhary@users.noreply.github.com>
2019-11-19 12:42:03 +0100

    Summary metrics (#1)
    * add summary metrics
    * add namespace and subsystem to metrics

--- main.go
+++ main.go
@@ -10,50 +10,75 @@ import (
 )
 
 var (
-	uptimeInSeconds = prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "uptime_in_seconds",
-		Help: "The uptime of this process in seconds",
+	uptimeInSecondsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "predictable",
+		Subsystem: "demo",
+		Name:      "uptime_in_seconds_counter",
+		Help:      "The uptime of this process in seconds",
 	})
 
-	currentMinute = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-		Name: "current_minute",
-		Help: "The current minute of this hour",
+	currentMinuteGauge = prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Namespace: "predictable",
+		Subsystem: "demo",
+		Name:      "current_minute_gauge",
+		Help:      "The current minute of this hour",
 	}, func() float64 { return float64(time.Now().Minute()) })
 
-	secondsDistribution = prometheus.NewHistogram(prometheus.HistogramOpts{
-		Name:    "seconds_distribution",
-		Help:    "The distribution of seconds across runtime",
-		Buckets: prometheus.LinearBuckets(10, 10, 5),
+	secondsDistributionHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Namespace: "predictable",
+		Subsystem: "demo",
+		Name:      "seconds_distribution_histogram",
+		Help:      "The distribution of seconds across runtime",
+		Buckets:   prometheus.LinearBuckets(10, 10, 5),
+	})
+
+	secondsDistributionSummary = prometheus.NewSummary(prometheus.SummaryOpts{
+		Namespace:  "predictable",
+		Subsystem:  "demo",
+		Name:       "seconds_distribution_summary",
+		Help:       "The distribution of seconds across runtime",
+		Objectives: map[float64]float64{},
 	})
 )
 
-func recordMetricUptimeInSeconds() {
+func recordMetricCounter() {
 	go func() {
 		ticker := time.Tick(time.Second)
 		for range ticker {
-			uptimeInSeconds.Inc()
+			uptimeInSecondsCounter.Inc()
 		}
 	}()
 }
 
-func recordMetricSecondsDistribution() {
+func recordMetricHistogram() {
 	go func() {
 		ticker := time.Tick(time.Second)
 		for now := range ticker {
-			secondsDistribution.Observe(float64(now.Second()))
+			secondsDistributionHistogram.Observe(float64(now.Second()))
+		}
+	}()
+}
+
+func recordMetricSummary() {
+	go func() {
+		ticker := time.Tick(time.Second)
+		for now := range ticker {
+			secondsDistributionSummary.Observe(float64(now.Second()))
 		}
 	}()
 }
 
 func main() {
-	recordMetricUptimeInSeconds()
-	recordMetricSecondsDistribution()
+	recordMetricCounter()
+	recordMetricHistogram()
+	recordMetricSummary()
 
 	registry := prometheus.NewRegistry()
-	registry.MustRegister(uptimeInSeconds, currentMinute, secondsDistribution)
+	registry.MustRegister(uptimeInSecondsCounter, currentMinuteGauge, secondsDistributionHistogram, secondsDistributionSummary)
 
 	metricsHandler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 	http.Handle("/metrics", metricsHandler)
 
+	log.Println("serving metrics at http://localhost:2112/metrics")
 	log.Fatal(http.ListenAndServe(":2112", nil))
 }
